b0e7933ef849d8c6693e1beaac8647f228d114ac
MatiMoringa <110281834+MatiMoringa@users.noreply.github.com>
2022-09-03 14:32:21 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY=Developer
+
+export MAIL_USERNAME=newweatherforecast@gmail.com
+export MAIL_PASSWORD=moringa123.
+export API_Key=8ed63fa07304e985c8d0c89880bc2bc7
+
+python3.8 manage.py server
